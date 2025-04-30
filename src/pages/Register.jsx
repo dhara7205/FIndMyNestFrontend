aa64df070f03f335bbea82c624182b8ae65c1e99
Dhara Patel <dharapatel2151@gmail.com>
2025-05-01 02:17:05 +0530

    atlas

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -36,7 +36,7 @@ const Register = () => {
     }
 
     try {
-      const res = await fetch(`${import.meta.env.VITE_API_BASE}/users/register`, {
+      const res = await fetch(`${import.meta.env.VITE_API_BASE}/api/users/register`, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(formData)
